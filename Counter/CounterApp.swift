ed06e64c3ac3f9d64e9389c5998232f939fb9447
Grzegorz Baranowski <ferrarto@op.pl>
2020-12-28 21:22:17 +0100

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Grzegorz Baranowski on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
